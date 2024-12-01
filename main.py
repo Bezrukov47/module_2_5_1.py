7b559afa755a52645e971fe0c5c9aa27436cd479
Bezrukov47 <bezr47@gmail.com>
2024-12-01 21:40:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+result_matrix = get_matrix(5, 8, 9)
+print(result_matrix)
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1, result2, result3)
+
